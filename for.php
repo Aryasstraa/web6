9c35e2409337d4f339dd78e97a780ec7ba4142ec
Aryasstraa <105297161+Aryasstraa@users.noreply.github.com>
2023-05-08 18:43:07 +0800

    Add files via upload

---
+++ for.php
@@ -0,0 +1,24 @@
+<?php
+
+for ($a=1; $a>=-10; $a--){
+    echo "Baris ke-$a <br />";
+}
+
+?>
+
+<!-- $a=1 = inisialisasi nilai,, 
+$a<=10 = desision klo benar akan dijalankan perulangan jika tidak maka perulangan akan berhenti, 
+$a++ Incremen menambah 1 dari sebelumnya, 
+$a-- decremen akan mengurangkan 1 dari sebelumnya -->
+
+<>
+<?php
+
+$awal = 1;
+while($awal <= 10){
+    echo " Nilai Sekarang adalah $awal <br/>";
+    $awal++;
+}
+
+
+?>
